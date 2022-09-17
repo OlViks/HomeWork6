5cc46f0f0a4cb4d43f61ea378f34a999c7e167aa
OlViks <salova.40@yandex.ru>
2022-09-18 00:04:23 +0300

    задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 import java.sql.SQLOutput;
-
-public class Main {
-    public static void main(String[] args) {System.out.println("Задание 6!");
+public class Main {    public static void main(String[] args) {
+        System.out.println("Задание 6!");
         //Задание 1
-        int clientOS = 1;
-        switch (clientOS) {
-            case 0:
-                System.out.println("Установите версию приложения для iOS по ссылке.");
-                break;
-            case 1:
-                System.out.println("Установите версию приложения для Android по ссылке.");
-                break;
-            default:
+        int clientOS = 2;
+        int oldSmart = 2020;
+        if (clientOS == 0 && oldSmart <= 2015) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке.");
+        } else if (clientOS == 0 && oldSmart > 2015) {
+            System.out.println("Установите версию приложения для iOS по ссылке.");
         }
-
+        if (clientOS == 1 && oldSmart <= 2015) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке.");
+        } else if (clientOS == 1 && oldSmart > 2015) {
+            System.out.println("Установите версию приложения для Android по ссылке.");
+        }
     }
 }
