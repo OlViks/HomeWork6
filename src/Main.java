6149fc5208ce78bc915d7c5e1fb63d985880a482
OlViks <salova.40@yandex.ru>
2022-09-17 23:31:13 +0300

    задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
+import java.sql.SQLOutput;
 
-        System.out.println("Задание 6!");
+public class Main {
+    public static void main(String[] args) {System.out.println("Задание 6!");
+        //Задание 1
+        int clientOS = 1;
+        switch (clientOS) {
+            case 0:
+                System.out.println("Установите версию приложения для iOS по ссылке.");
+                break;
+            case 1:
+                System.out.println("Установите версию приложения для Android по ссылке.");
+                break;
+            default:
+        }
+
     }
 }
